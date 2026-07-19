941452c2a3836c9a2d53bd677b6f0d1d3dfd1b2e
K Ramya <kramya1368@gmail.com>
2026-07-19 14:50:06 +0530

    Time: 3 ms (86.04%) | Memory: 13.7 MB (39.02%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for x in set(nums):
+            if nums.count(x)>len(nums)//2:
+                return x
