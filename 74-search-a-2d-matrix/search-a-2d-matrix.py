55f465b810aaffa3a425460290090c8f1d90087c
K Ramya <kramya1368@gmail.com>
2026-07-20 08:08:57 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (30.11%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -5,8 +5,23 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-        for row in matrix:
-            for col in row:
-                if target==col:
-                    return True
-        return False            
+        rows=len(matrix)
+        cols=len(matrix[0])
+
+        low=0
+        high=(rows*cols)-1
+
+        while low <=high:
+            mid=(low+high)//2
+
+            row=mid//cols
+            col=mid%cols
+            middle_value=matrix[row][col]
+
+            if middle_value==target:
+                return True
+            elif middle_value<target:
+                low=mid+1
+            else:
+                high=mid-1        
+        return False
