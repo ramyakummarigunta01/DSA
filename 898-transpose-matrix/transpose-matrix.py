4c4069b703b5e5c65e48ac6093e3885d5e710c2b
K Ramya <kramya1368@gmail.com>
2026-07-20 18:15:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (76.18%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.py
+++ 898-transpose-matrix/transpose-matrix.py
@@ -4,4 +4,10 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[List[int]]
         """
-        return [list(row) for row in zip(*matrix)]
+        m=len(matrix)
+        n=len(matrix[0])
+        result=[[0]*m for _ in range(n)]
+        for i in range(m):
+            for j in range(n):
+                result[j][i]=matrix[i][j]
+        return result        
